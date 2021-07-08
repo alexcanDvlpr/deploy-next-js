51815534b8f61669f6f39f42110a5dc57c2f253e
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-07-08 13:37:39 +0200

    Add configuration

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    // Base Path
+    basePath: '/',
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? '/deploy-next-js/' : '',
+}
